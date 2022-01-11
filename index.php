3c66fbcdfdf0ec5c0c7ae5ca5bf4eba8655d508e
vindula97 <vindulanayanajith376@gmail.com>
2022-01-11 16:44:37 +0530

    Final commit

---
+++ index.php
@@ -0,0 +1,8 @@
+class StudentParent{
+            public $stdName;
+            public $stdRegNum;
+            function constructor($name, $regnum){
+                $this->stdName=$name;
+                $this->stdRegNum=$regnum;
+
+            }
